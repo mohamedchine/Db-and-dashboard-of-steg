ab693d462398a3f77fa82ca75d1b2c0a5e85e081
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2025-04-08 22:25:12 +0100

    x
    x

--- frontend/src/pages/welcomepage/components/signup/signup.js
+++ frontend/src/pages/welcomepage/components/signup/signup.js
@@ -1,7 +1,28 @@
+import { useState } from "react";
 const Signup = () => {
+    var unittypes = ["central" ,"groupement","direction"];
+    const [fn, setfn] = useState("");
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+    const [unit, setunit] = useState("");
+    const [specify, setspecify] = useState("");
     return ( 
         <div className="signup">
-            
+             <input type="text"  placeholder=" FullName"  className="fname" />
+             <input type="text"  placeholder="Steg email "  className="se" />
+             <input type="password" className="pas" placeholder="Password" />
+             <select className="unit">
+                 <option value="unit">Select unit type</option>   
+                    {unittypes.map((unit) => (
+                        <option value={unit} >
+                            {unit}
+                        </option>
+                    ))}
+             </select>
+             <select className="specify unit">
+               select {unit}
+
+             </select>
         </div>
      );
 }
