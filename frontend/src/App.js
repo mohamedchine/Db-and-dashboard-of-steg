af858e5f3a7ccd82dbe16d1fb4a6c16523bb0cef
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2025-04-14 18:03:53 +0100

    default route
    added default route to oppen the page on

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,11 +8,14 @@ import Wpage from "./pages/welcomepage/welcomepage";
 import NotFound from "./pages/notfound/notfound";
 import RequestResetPassword from "./pages/welcomepage/components/forgetpassword/requestresetpassword";
 import Resetpassword from "./pages/welcomepage/components/reset-password-component/resetpassword";
+import { Navigate } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
       <Routes>
+        {/* to first when we oppen the app it goes to welcomepage  replace to not be able to go back to /  and show 404*/}
+      <Route path="/" element={<Navigate to="/welcomepage" replace />} />   
           <Route path="/welcomepage" element={<Wpage />}>
             <Route index element={<Home />} />
             <Route path="home" element={<Home />} />
