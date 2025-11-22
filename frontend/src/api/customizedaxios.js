4c02a2f3f90ba233cc29d279b11b21c15bfc2f17
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2025-11-22 22:23:26 +0100

    Update customizedaxios.js

--- frontend/src/api/customizedaxios.js
+++ frontend/src/api/customizedaxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 const baseURL = process.env.REACT_APP_API_BASE_URL;
-console.log(baseURL);
+console.log('the base url is :',baseURL);
 const axs = axios.create({
   
   baseURL: process.env.REACT_APP_API_BASE_URL ,
