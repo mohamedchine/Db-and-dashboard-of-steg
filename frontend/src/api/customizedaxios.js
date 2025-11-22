2c86b6baf9aab81086460d454766aa0bb4faa2dc
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2025-11-22 03:03:15 +0100

    frontend connecting to deployement backend
    meow

--- frontend/src/api/customizedaxios.js
+++ frontend/src/api/customizedaxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const axs = axios.create({
-  baseURL: 'http://localhost:3004/',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3004/',
   withCredentials: true,
 });
 
