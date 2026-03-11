a07be3bf677bc7400fce78e543a05970adab0982
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2026-03-11 02:43:10 +0000

    updating mailing utils

--- backend/utils/mailUtils.js
+++ backend/utils/mailUtils.js
@@ -1,35 +1,22 @@
-const nodemailer = require('nodemailer') ;
-const  {genjwt , verifyjwt} = require('../utils/jwtUtils');
-const sendmail = async (email, subject, content) => {
-    const transport = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: 'koorahubb@gmail.com', //temp mail just for testing
-            pass: 'kvro xetd wnka zfzu',
-        },
-        connectionTimeout: 60000,
-        socketTimeout: 60000,
-        greetingTimeout: 30000,
+const axios = require('axios');
+
+const sendEmail = async (userEmail, subject, htmlTemplate) => {
+  try {
+    await axios.post('https://api.brevo.com/v3/smtp/email', {
+      sender: { name: 'Steg', email: process.env.APP_EMAIL_ADDRESS },
+      to: [{ email: userEmail }],
+      subject,
+      htmlContent: htmlTemplate,
+    }, {
+      headers: {
+        'api-key': process.env.BREVO_API_KEY,
+        'Content-Type': 'application/json',
+      }
     });
-
-    const mailoptions = {
-        from: 'koorahubb@gmail.com', 
-        to: email,
-        subject: subject,
-        html: content,
-    };
-
-    await transport.sendMail(mailoptions);
+  } catch (err) {
+    console.error("couldn't send email", err);
+    throw err;
+  }
 };
 
-const gen_email_verification_Link = (email , id)=>{
-      token = genjwt({email,id},'15m') ; 
-        const link = process.env.BACKEND_URL+'/auth/verify-email?token='+token;
-        return link ; 
-
-}
-
-module.exports = {sendmail,gen_email_verification_Link}
-
+module.exports = sendEmail;
