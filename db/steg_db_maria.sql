177891920e199aa5cb6ac2ef14b949d62c08a313
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2026-03-11 02:28:57 +0000

    adding scripts in package.json

--- db/steg_db_maria.sql
+++ db/steg_db_maria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2025 at 01:34 AM
+-- Generation Time: Dec 09, 2025 at 09:52 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
