5090f3e7c7421c37ddc78ae198c17dd3eed0dad1
mohamedchine <120230689+mohamedchine@users.noreply.github.com>
2026-07-23 23:09:48 +0100

    Refactor database connection to use DATABASE_URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,3 @@
 const sql = require('mysql2/promise');
-
-const db = sql.createPool({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        port: process.env.DB_PORT
-    });    
-module.exports = db  ;
+const db = sql.createPool(process.env.DATABASE_URL);
+module.exports = db;
